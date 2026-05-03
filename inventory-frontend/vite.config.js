8641ce4c4e1b89c45e12c8cec082f7e51cc2f102
cengiztansel <cengiz_tansel@yahoo.com.tr>
2026-05-03 03:13:37 +0300

    Rehearsal commit

--- inventory-frontend/vite.config.js
+++ inventory-frontend/vite.config.js
@@ -4,11 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+/*
     server: {
       allowedHosts: ["cengiztansel.duckdns.org"], // Bu satırı ekle
       host: true, // Dış dünyaya açılması için şart
       port: 5173
+    }
+*/
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      allowedHosts: true // Tüm hostlara izin ver (Test aşaması için en garantisi budur)
     }
 
 })
