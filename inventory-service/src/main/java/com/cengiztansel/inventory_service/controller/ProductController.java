fc6ccef9f7137649f9c2c2dfec3379ccf0877396
cengiztansel <cengiz_tansel@yahoo.com.tr>
2026-05-07 17:13:16 +0300

    update

--- inventory-service/src/main/java/com/cengiztansel/inventory_service/controller/ProductController.java
+++ inventory-service/src/main/java/com/cengiztansel/inventory_service/controller/ProductController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://cengiztansel.duckdns.org:5173")
 public class ProductController {
 
     private final ProductRepository productRepository;
